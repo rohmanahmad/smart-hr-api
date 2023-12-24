81069502666fc5aff2e410c3466452ac14e679f9
rohmanahmad <rohmanmail@gmail.com>
2023-12-24 09:39:37 +0700

    update relation

--- database/migrations/2023-12-20/008_client_admins.ts
+++ database/migrations/2023-12-20/008_client_admins.ts
@@ -6,6 +6,7 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('companyCode', 3).notNullable().references('companies.code')
       table
         .string('clientCode', 3)
         .unique()
